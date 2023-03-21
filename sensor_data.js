5598a734d363ad4700538de7884f0cd1d42b70f5
DeroMal <derrickmla123@gmail.com>
2023-03-21 16:28:30 +0300

    debug mysql connection: Test 2

--- sensor_data.js
+++ sensor_data.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 // Database connection
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'wlgykctu',
+    user: 'wlgykctu_derrickml',
     password: 'derrickloma',
     database: 'wlgykctu_smartgro'
 });
