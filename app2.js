a3a255422c33d2ace2cc0d8faab47826413c8ee5
DeroMal <derrickmla123@gmail.com>
2023-03-22 21:07:02 +0300

    table pagination created on the table page

--- app2.js
+++ app2.js
@@ -2,9 +2,61 @@ require("dotenv").config();
 const express = require('express');
 const cors = require("cors");
 const path = require('path');
+const mysql = require('mysql');
+const ejs = require('ejs');
+
 const app = express();
+
+// Set up MySQL connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'wlgykctu_derrickml',
+  password: 'derrickloma',
+  database: 'senwlgykctu_smartgro'
+});
+
+// Connect to MySQL
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL database');
+});
+
+// Set up middleware for serving static files and parsing request body
+app.use(express.static('public/pages/pages'));
+app.use(express.urlencoded({ extended: true }));
+
+// Set up route for displaying paginated sensor data
+app.get('/table', (req, res) => {
+  const pageSize = 10; // number of records to display per page
+  const currentPage = req.query.page || 1; // get current page from query parameter or default to page 1
+  const startIndex = (currentPage - 1) * pageSize; // calculate start index of records to display
+
+  // Query the database for sensor data
+  const query = `SELECT * FROM sensor_data ORDER BY id DESC LIMIT ${startIndex},${pageSize}`;
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    const sensorData = results;
+    const totalCountQuery = 'SELECT COUNT(*) AS totalCount FROM sensor_data';
+    connection.query(totalCountQuery, (err, result) => {
+      if (err) throw err;
+      const totalCount = result[0].totalCount;
+      const pageCount = Math.ceil(totalCount / pageSize);
+      res.render('table', {
+        sensorData,
+        pageCount,
+        currentPage,
+      });
+    });
+  });
+});
+
+// Set up view engine and template directory
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+//---------------------------------------------------//
 // Serve the HTML and JavaScript files
-app.use(express.static('public/pages'));
+// app.use(express.static('public/pages'));
 
 // Start the server
 const port = 3000;
--- app3.js
+++ app3.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const mysql = require('mysql');
+const ejs = require('ejs');
 
 const app = express();
 
-// Set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// Create a connection to the database
+// Set up MySQL connection
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -14,36 +12,44 @@ const connection = mysql.createConnection({
   database: 'sensgro'
 });
 
-// Connect to the database
-connection.connect();
-
-// Define a route for the homepage
-app.get('public/views/table', (req, res) => {
-  // Query the database for temperature and humidity data
-  connection.query('SELECT * FROM sensor_data', (error, results, fields) => {
-    if (error) throw error;
-    // Render the index view with the temperature and humidity data
-    res.render('table', { data: results });
-  });
+// Connect to MySQL
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL database');
 });
 
-// Define a route for the temperature and humidity graph
-app.get('/graph', (req, res) => {
-  // Query the database for temperature and humidity data
-  connection.query('SELECT * FROM sensor_data', (error, results, fields) => {
-    if (error) throw error;
+// Set up middleware for serving static files and parsing request body
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
-    // Extract the temperature and humidity data from the results
-    const temperatures = results.map(result => result.temperature);
-    const humidities = results.map(result => result.humidity);
+// Set up route for displaying paginated sensor data
+app.get('/table', (req, res) => {
+  const pageSize = 10; // number of records to display per page
+  const currentPage = req.query.page || 1; // get current page from query parameter or default to page 1
+  const startIndex = (currentPage - 1) * pageSize; // calculate start index of records to display
 
-    // Render the graph view with the temperature and humidity data
-    res.render('graph', {
-      temperatures: temperatures,
-      humidities: humidities
+  // Query the database for sensor data
+  const query = `SELECT * FROM sensor_data ORDER BY id DESC LIMIT ${startIndex},${pageSize}`;
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    const sensorData = results;
+    const totalCountQuery = 'SELECT COUNT(*) AS totalCount FROM sensor_data';
+    connection.query(totalCountQuery, (err, result) => {
+      if (err) throw err;
+      const totalCount = result[0].totalCount;
+      const pageCount = Math.ceil(totalCount / pageSize);
+      res.render('table', {
+        sensorData,
+        pageCount,
+        currentPage,
+      });
     });
   });
 });
+
+// Set up view engine and template directory
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
 // Start the server
 app.listen(3000, () => {
