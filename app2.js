d2fdfb61cdc9891d473f5a6a0d78766f81b2a5b8
DeroMal <derrickmla123@gmail.com>
2023-03-24 14:59:09 +0300

    change of view directory

--- app2.js
+++ app2.js
@@ -23,11 +23,12 @@ connection.connect((err) => {
 });
 
 // Set up middleware for serving static files and parsing request body
-app.use(express.static('../public/pages/'));
+// app.use(express.static('../public/pages/'));
+app.use(express.static('/'));
 app.use(express.urlencoded({ extended: true }));
 
 // Set up route for displaying paginated sensor data
-app.get('/', (req, res) => {
+app.get('/table', (req, res) => {
   const pageSize = 10; // number of records to display per page
   const currentPage = req.query.page || 1; // get current page from query parameter or default to page 1
   const startIndex = (currentPage - 1) * pageSize; // calculate start index of records to display
