54e0508b1fa076f0aef3ae95c2d8d74c18385250
DeroMal <derrickmla123@gmail.com>
2023-03-31 12:18:45 +0300

    dynamic temp display test 8

--- public/assets/js/tval.js
+++ public/assets/js/tval.js
@@ -1,14 +1,7 @@
-// Get the temperature element from the HTML
-const temperatureElement = document.getElementById('tval');
-
-// Fetch the latest sensor data from the server
 fetch('/public/pages')
   .then(response => response.json())
   .then(data => {
-    // Extract the temperature value from the data
-    const temperature = data.temperature;
-
-    // Update the temperature element with the new value
-    temperatureElement.textContent = temperature;
+    document.getElementById('tval').textContent = data.temperature;
+    // document.getElementById('humidity').textContent = data.humidity;
   })
   .catch(error => console.error(error));
