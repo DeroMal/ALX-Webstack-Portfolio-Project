d401d9828fa81e7142e2967133339fb42fef5a98
DeroMal <derrickmla123@gmail.com>
2023-04-11 01:49:45 +0300

    enableing cross-origin second attempt

--- public/assets/js/chart_sensor.js
+++ public/assets/js/chart_sensor.js
@@ -328,3 +328,15 @@ chartLight = new Chart(
         },
     }
 );
+
+setInterval(() => {
+    fetch('/chart-data2')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('tval').textContent = data.temperatureData.temperature[0];
+            document.getElementById('hval').textContent = data.humidityData.humidity[0];
+            // document.getElementById('mval').textContent = data.moisture[0];
+            document.getElementById('lval').textContent = data.lightData.light[0];
+        })
+        .catch(error => console.error(error));
+}, 2000);
