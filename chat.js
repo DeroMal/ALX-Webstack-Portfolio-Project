0434bf1752cf3bc0316a77f9e17fe161ebf97662
DeroMal <derrickmla123@gmail.com>
2023-04-17 23:32:43 +0300

    running test chatbot

--- chat.js
+++ chat.js
@@ -24,45 +24,33 @@ fs.createReadStream(csvFilePath)
     })
     .on('end', () => {
         console.log('Accessing sensor data at real time ... \nSuccessfully processed database data\n');
-        runBot();
     });
 
-// Run the chat bot continuously
-async function runBot() {
-    while (true) {
-        // Prompt the user to enter a question
-        const question = await prompt("Ask a question: ");
+// Handle GET requests to /chatbot endpoint
+app.get('/chatbot', async(req, res) => {
+    try {
+        // Get the user's question from the query parameter
+        const question = req.query.question;
 
-        try {
-            // Get the last row of the CSV data
-            const lastRow = data[data.length - 1];
+        // Get the last row of the CSV data
+        const lastRow = data[data.length - 1];
 
-            // Generate the bot's response
-            const response = await openai.createCompletion({
-                model: "text-davinci-003",
-                prompt: `${question}\n${Object.values(lastRow).join('\n')}\n`,
-                max_tokens: 500,
-            });
+        // Generate the bot's response
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${question}\n${Object.values(lastRow).join('\n')}\n`,
+            max_tokens: 500,
+        });
 
-            // Print the bot's response
-            console.log("Caelum:", response.data.choices[0].text.trim());
-        } catch (error) {
-            console.error("Error while running completion:", error.message);
-        }
+        // Send the bot's response back to the client
+        res.send(response.data.choices[0].text.trim());
+    } catch (error) {
+        console.error("Error while running completion:", error.message);
+        res.status(500).send("An error occurred while processing your request.");
     }
-}
+});
 
-// Helper function to prompt the user for input
-function prompt(question) {
-    return new Promise((resolve, reject) => {
-        const readline = require('readline').createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-
-        readline.question(question, (answer) => {
-            readline.close();
-            resolve(answer);
-        });
-    });
-}
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
