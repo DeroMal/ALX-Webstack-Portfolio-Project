69380244387fedd058f3a8fc0e5f72a8da37e4c7
DeroMal <derrickmla123@gmail.com>
2023-04-18 15:52:31 +0300

    ui change

--- chat.js
+++ chat.js
@@ -51,9 +51,16 @@ app.get('/chat', async(req, res) => {
                 // Generate the bot's response
                 const jsonData = results.map(result => result.json_data);
                 const response = await openai.createCompletion({
-                    model: "text-davinci-003",
-                    prompt: `${req.query.question}\n\n${jsonData.join('\n')}\n`,
-                    max_tokens: 100,
+                    // model: "text-davinci-003",
+                    // prompt: `${req.query.question}\n\n${jsonData.join('\n')}\n`,
+                    // max_tokens: 100,
+                    prompt: "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: ",
+                    temperature: 0.9,
+                    max_tokens: 150,
+                    top_p: 1,
+                    frequency_penalty: 0,
+                    presence_penalty: 0.6,
+                    stop: [" Human:", " AI:"],
                 });
 
                 // Return the bot's response
