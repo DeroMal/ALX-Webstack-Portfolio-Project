a191e9475a18305c8d7f5ed6ce7996d6f7ace837
DeroMal <derrrickmal123@gmail.com>
2023-04-18 06:28:07 -0400

    raised issue

--- chat.js
+++ chat.js
@@ -29,7 +29,7 @@ fs.createReadStream(csvFilePath)
 // Serve the web interface
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'chat.html'));
 });
 
 // Handle chat bot requests
