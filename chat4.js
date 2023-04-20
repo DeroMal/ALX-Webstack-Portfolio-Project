e58f05f73c6ecc2ca2f001623e7e7a5efd060394
DeroMal <derrickmla123@gmail.com>
2023-04-21 01:54:30 +0300

    gpt api integration

--- chat4.js
+++ chat4.js
@@ -1,69 +1,81 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
-const openai = require('openai');
-
-// Set your OpenAI API key
-openai.apiKey = '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';
-
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const path = require('path');
+const openai = require('openai');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const mysql = require('mysql');
+
+// Set up OpenAI API key
+openai.apiKey = process.env.OPENAI_API_KEY;
 
 // Set up MySQL connection
-const dbCredentials = require('../db'); //Import file containing credentials
+const dbCredentials = require('./db'); // Import file containing credentials
 
 // Establish database connection using the credentials
-const db = mysql.createConnection({
+const connection = mysql.createConnection({
     host: dbCredentials.host,
     user: dbCredentials.user,
     password: dbCredentials.password,
     database: dbCredentials.database
 });
 
-// Connect to the database
-db.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to the database.');
-});
-
-// Endpoint to get sensor data and chat with ChatGPT model
-app.post('/chat', async(req, res) => {
-    const { question } = req.body;
-    if (!question) {
-        return res.status(400).json({ error: 'Please provide a question.' });
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL database:', err.message);
+        process.exit(1);
     }
-
-    const sensorDataQuery = 'SELECT temperature, humidity FROM sensor_data ORDER BY dateTime DESC LIMIT 50';
-    db.query(sensorDataQuery, async(err, results) => {
-        if (err) throw err;
-
-        let sensorDataText = 'Last 50 sensor readings: ';
-        results.forEach((result, index) => {
-            sensorDataText += `\nReading ${index + 1}: Temperature: ${result.temperature} °C, Humidity: ${result.humidity} %. `;
-        });
-
-        const prompt = `${sensorDataText}\n${question}`;
-
-        try {
-            const chatGPTResponse = await openai.Completion.create({
-                engine: 'text-davinci-002',
-                prompt: prompt,
-                max_tokens: 100,
-                n: 1,
-                stop: null,
-                temperature: 0.5,
-            });
-
-            const answer = chatGPTResponse.choices[0].text.trim();
-            res.json({ answer });
-        } catch (error) {
-            res.status(500).json({ error: 'Error processing your question.' });
-        }
-    });
+    console.log('Connected to MySQL database');
 });
 
+// Parse incoming JSON data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve the web interface
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'chat.html'));
+});
+
+// Handle chat bot requests
+app.post('/chat', async(req, res) => {
+    try {
+        // Query the MySQL database for the relevant data
+        const queryString = 'SELECT temperature, humidity FROM temp_humid ORDER BY dateTime DESC LIMIT 50';
+        connection.query(queryString, async(error, results, fields) => {
+            if (error) {
+                console.error('Error querying MySQL database:', error.message);
+                res.status(500).send('Error querying MySQL database');
+            } else {
+                // Format the sensor data into text
+                let sensorDataText = 'Last 50 sensor readings:';
+                results.forEach((result, index) => {
+                    sensorDataText += `\nReading ${index + 1}: Temperature: ${result.temperature} °C, Humidity: ${result.humidity} %.`;
+                });
+
+                // Include the sensor data in the prompt
+                const prompt = `${sensorDataText}\n${req.body.question}`;
+
+                // Generate the bot's response
+                const response = await openai.Completion.create({
+                    engine: 'text-davinci-003',
+                    prompt: prompt,
+                    max_tokens: 500,
+                });
+
+                // Return the bot's response
+                res.send(response.choices[0].text.trim());
+            }
+        });
+    } catch (error) {
+        console.error('Error while running completion:', error.message);
+        res.status(500).send('Error while running completion');
+    }
+});
+
+// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
 });
