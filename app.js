9ed7b8c6b6ff80d2b29b176684677f8f4f189994
DeroMal <derrickmla123@gmail.com>
2023-04-21 16:26:20 +0300

    Caelumchat added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 const mysql = require('mysql');
 const ejs = require('ejs');
 const moment = require('moment-timezone');
+const { Configuration, OpenAIApi } = require('openai');
 
 const timezone = 'Africa/Kampala';
 
@@ -23,6 +24,14 @@ app.get('/assets/js/:filename', (req, res) => {
     res.set('Content-Type', 'application/javascript');
     res.sendFile(path.join(__dirname, `/public/assets/js/${filename}`));
 });
+
+// Set up OpenAI API configuration
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+// Create OpenAI API instance
+const openai = new OpenAIApi(configuration);
 
 // Set up MySQL connection
 const dbCredentials = require('./db'); //Import file containing credentials
@@ -300,6 +309,65 @@ app.post('/actuator-data', (req, res) => {
         res.status(200).json({ message: 'Actuator data saved successfully' });
     });
 });
+
+/**====CHAT Establishment routes=================**/
+// Retrieve data from the database
+async function getDataFromDatabase() {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM temp_humid ORDER BY dateTime DESC LIMIT 50', (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
+
+// Configure Express app
+app.use(cors());
+app.use(express.json());
+
+// API endpoint for the chatbot
+app.post('/api/chat', async(req, res) => {
+    const question = req.body.question;
+
+    try {
+        // Get the last 50 rows of sensor data
+        const data = await getDataFromDatabase();
+
+        // Format the data as a table
+        const dataAsText = data.map(row => Object.values(row).join(', ')).join('\n');
+
+        // Add a brief description of the data
+        const dataDescription = `The table below shows the last 50 temperature and humidity sensor readings in the format "Timestamp, Temperature, Humidity":\n${dataAsText}\n`;
+
+        let responseText = "";
+        let attempts = 0;
+
+        // Retry until a valid response is generated or a maximum number of attempts is reached
+        while (responseText.trim().length < 10 && attempts < 5) {
+            // Generate the bot's response
+            const response = await openai.createCompletion({
+                model: "text-davinci-003",
+                prompt: `I am a knowledgeable Caelum (AI) with access to the following data:\n${dataDescription}\nUser: ${question}\nCaelum (AI):`,
+                max_tokens: 500,
+                n: 1,
+                stop: ["\n"],
+            });
+
+            responseText = response.data.choices[0].text.trim();
+            attempts++;
+        }
+
+        // Instead of printing the response, send it as a JSON object
+        res.json({ response: responseText });
+    } catch (error) {
+        console.error("Error while running completion:", error.message);
+        res.status(500).json({ error: error.message });
+    }
+});
+/**=======END CHAT ROUTE=========================**/
 
 // Set up view engine and template directory
 app.set('view engine', 'ejs');
